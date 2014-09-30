932b9c6c24483a1efce3be6e1b95f11ece7f3850
Henrik Feldt <henrik@haf.se>
2014-09-30 13:42:56 +0200

    more

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-
-./.osx
 xcode-select --install
+exec bash
 
-
+git clone https://github.com/Intelliplan/osx.git
+cd osx
+./.osx
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+cp .bash_profile .
+exec bash
+brew bundle
