bf02e09dbf4c1b6619e9eac1e876e1ff6f8f3aee
Kristoffer Roupé <kitofr@gmail.com>
2014-11-21 14:25:06 +0100

    Added back things
    - Dropbox
    - ssh_keys
    - dotfiles
    - powerline

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 
-task :default => [:xcode, :osx, :brews, :casks, :zshell, :git_config, :computer_name]
+task :default => [:xcode, :osx, :brews, :casks, :zshell, :ssh_keys, :dotfiles, :powerline, :pip, :git_config, :computer_name]
 
 def curl what
   sh "curl -O #{what}" 
@@ -38,6 +38,9 @@ def ask_for what
   STDIN.gets.strip
 end
 
+def npm what
+  sh "npm install -g #{what}"
+end
 
 desc "Installs xcode. Waits for input while installer is running"
 task :xcode do
@@ -76,12 +79,51 @@ end
 
 desc "Installs common casks"
 task :casks do
-  %w[mou teamviewer spectacle royal-tsx parallels onepassword bittorrent-sync 
+  %w[mou teamviewer dropbox spectacle royal-tsx parallels onepassword bittorrent-sync 
     firefox caffeine colloquy gpgtools virtualbox vagrant iterm2 adium vlc
      disk-inventory-x hipchat spotify flux ].each do |c|
     cask c
   end
   sh "brew tap caskroom/fonts"
+end
+
+
+desc "Installs global npm packages"
+task :npms do
+  %w[neochrome/marky bower].each do |p|
+    npm p
+  end  
+end
+
+task :ssh_keys do
+  puts "Please sign in to your dropbox folder before you continue"
+  STDIN.gets.strip
+  home = Dir.home
+  Dir.mkdir "#{home}/.ssh" unless Dir.exists? "#{home}/.ssh"
+  soft_link "#{home}/Dropbox/Private/ssh/github_rsa", "#{home}/.ssh/github_rsa"
+  sh "chmod 400 ~/.ssh/github_rsa"
+  soft_link "#{home}/Dropbox/Private/ssh/id_rsa", "#{home}/.ssh/id_rsa"
+  sh "chmod 400 ~/.ssh/id_rsa"
+end
+
+task :dotfiles do
+  `git clone https://github.com/kitofr/dotfiles.git` unless Dir.exists? "#{Dir.home}/dotfiles"
+  in_dir "dotfiles" do
+    sh "sudo gem install bundler"
+    sh "bundle"
+    sh "bundle exec rake"
+  end
+end
+
+task :powerline do
+  `git clone https://github.com/Lokaltog/powerline-fonts.git`
+  in_dir "powerline-fonts" do
+    sh "find . -name '*.[o,t]tf' -type f -print0 | xargs -0 -I % cp % $HOME/Library/Fonts/"
+  end
+end
+
+task :pip do
+  sh "pip install pygments"
 end
 
 desc "Installs Oh-my zshell"
