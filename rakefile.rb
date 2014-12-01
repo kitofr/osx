bcd7c0a906cbb079fbda239f9c76ed1c8f624295
Kristoffer Roupe <kristoffer.roupe@intelliplan.se>
2014-12-01 10:53:09 +0100

    Added htop and weechat

--- rakefile.rb
+++ rakefile.rb
@@ -70,7 +70,7 @@ task :brews do
   sh "brew upgrade"
   %w[awscli git vcsh mr jq ack openssl tree ucspi-tcp readline rbenv ruby-build 
     nginx python python3 erlang tsung nmap sqlmap ngrep node mc mutt postgresql
-    rbenv-gem-rehash leiningen wget tmux elixir elixir-build].each do |r|
+    htop weechat rbenv-gem-rehash leiningen wget tmux elixir elixir-build].each do |r|
     brew r
   end
   brew "imagemagick --with-webp"
